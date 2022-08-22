64e994707afb67e3b5b339c29b0cd0a0908fa29d
Laurentiu Borza <borza.laurentiu@gmail.com>
2022-08-22 21:24:26 +0100

    adding the script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+  sudo apt-get update
+  sudo apt-get upgrade -y
   sudo apt update
-  sudo apt install apache2 -y
-  sudo chown ubuntu /var/www/html/index.html
-  sudo chmod 774 /var/www/html/index.html
-  sudo echo "<h1>Hello</h1>" > /var/www/html/index.html
+  sudo apt-get update
+  sudo apt-get install ansible -y
+  # sudo apt install apache2 -y
+  # sudo chown ubuntu /var/www/html/index.html
+  # sudo chmod 774 /var/www/html/index.html
+  # sudo echo "<h1>Hello</h1>" > /var/www/html/index.html]
