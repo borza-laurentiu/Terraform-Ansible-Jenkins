9d04f1416e1a896232c19c632422252160f9724c
borza-laurentiu <35265131+borza-laurentiu@users.noreply.github.com>
2023-03-14 08:34:28 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@
   # sudo apt install apache2 -y
   # sudo chown ubuntu /var/www/html/index.html
   # sudo chmod 774 /var/www/html/index.html
-  # sudo echo "<h1>Hello</h1>" > /var/www/html/index.html]
+  # sudo echo "<h1>Hello</h1>" > /var/www/html/index.html
